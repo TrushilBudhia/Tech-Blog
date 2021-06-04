a6c65019b90572a5fb745396e2d9d1def124eb59
Trushil Budhia <trushil.budhia@gmail.com>
2021-06-04 09:30:05 +0800

    Added models User.js and index.js, updated connection.js, controllers home-routes.js, api user-routes.js and server.js - application connects to server and creates the User table successfully

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,12 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: process.env.DB_PORT,
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
 module.exports = sequelize;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,34 +1,34 @@
 const router = require('express').Router();
-const { Post, Comment } = require('../models');
+const { Post } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
-      const dbPostData = await Post.findAll({
-        include: [
-          {
-            model: Comment,
-            attributes: [
-                'id',
-                'comment_text',
-                'post_id', 
-                'user_id', 
-                'created_at'
-            ],
-          },
-        ],
-      });
-  
-      const posts = dbPostData.map((post) =>
-        post.get({ plain: true })
-      );
-  
-      res.render('homepage', {
-        posts,
-        loggedIn: req.session.loggedIn,
-      });
+        const dbPostData = await Post.findAll({
+            // include: [
+            //     {
+            //         model: Comment,
+            //         attributes: [
+            //             'id',
+            //             'comment_text',
+            //             'post_id',
+            //             'user_id',
+            //             'created_at'
+            //         ],
+            //     },
+            // ],
+        });
+
+        const posts = dbPostData.map((post) =>
+            post.get({ plain: true })
+        );
+
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
     } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+        console.log(err);
+        res.status(500).json(err);
     }
 });
 
