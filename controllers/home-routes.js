9bd09aabb1a10ef4f4f3ba344c2f5a1a97881d4d
Trushil Budhia <trushil.budhia@gmail.com>
2021-06-06 14:54:12 +0800

    Updated home-routes.js - added router for individual post and login (GET request)

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -20,6 +20,10 @@ router.get('/', async (req, res) => {
             //             'user_id',
             //             'created_at'
             //         ],
+            //         include: {
+            //             model: User,
+            //             attributes: ['username'],
+            //         },
             //     },
             //     {
             //         model: User,
@@ -40,6 +44,64 @@ router.get('/', async (req, res) => {
         console.log(err);
         res.status(500).json(err);
     }
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const dbPostData = await Post.findByPk(req.params.id, {
+            where: {
+                id: req.params.id,
+            },
+            attributes: [
+                'id',
+                'title',
+                'content',
+                'created_at'
+            ],
+            // include: [
+            //     {
+            //         model: Comment,
+            //         attributes: [
+            //             'id',
+            //             'comment_text',
+            //             'post_id',
+            //             'user_id',
+            //             'created_at'
+            //         ],
+            //         include: {
+            //             model: User,
+            //             attributes: ['username'],
+            //         },
+            //     },
+            //     {
+            //         model: User,
+            //         attributes: [ 'username' ],
+            //     },
+            // ],
+        });
+        if (!dbPostData) {
+            res
+                .status(404)
+                .json({ message: 'No post found with that id' });
+            return;
+        }
+        const posts = dbPostData.map((post) =>
+            post.get({ plain: true })
+        );
+        res.render('individual-post', { post, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
