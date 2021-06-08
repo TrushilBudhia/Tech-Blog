84ff7cf85de252953713be79d7e3186596365a32
Trushil Budhia <trushil.budhia@gmail.com>
2021-06-08 11:33:44 +0800

    Updated handlebars template files - added dashboard and add new post templates and js file. Updated style.css

---
+++ public/js/add-new-post.js
@@ -0,0 +1,25 @@
+const addPostFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#post-title').value.trim();
+    const content = document.querySelector('#post-content').value.trim();
+  
+    if (title && content) {
+      const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      // Redirecting user to the dashboard if response is ok
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create post.');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.add-post-form')
+    .addEventListener('submit', newPostFormHandler);
+  
