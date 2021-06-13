0de958923d20e8313f03ed9c07ccc79b36cdf0f6
Trushil Budhia <trushil.budhia@gmail.com>
2021-06-13 20:41:21 +0800

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,12 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: 3306,
+    port: process.env.DB_PORT,
+    dialectOptions: {
+      decimalNumbers: true,
+    },
   });
 
 module.exports = sequelize;
