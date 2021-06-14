d2a38c91482977576a2574a1a2e9ba9e984b9e05
Trushil Budhia <trushil.budhia@gmail.com>
2021-06-14 22:54:59 +0800

    Updated add-new-post.js

--- public/js/add-new-post.js
+++ public/js/add-new-post.js
@@ -1,38 +1,36 @@
 const addPostFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const title = document.querySelector('#post-title').value;
-    const content = document.querySelector('#post-content').value;
-    console.log('title', title);
-    console.log('content', content);
-    if (title && content) {
-      const response = await fetch('/api/posts', {
-        method: 'POST',
-        body: JSON.stringify({ title, content }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      // Redirecting user to the dashboard if response is ok
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        const error = `<p>*Failed to create post</p>`;
-        document.querySelector('.error-message').innerHTML = error;
-      }
+  event.preventDefault();
+
+  const title = document.querySelector('#post-title').value;
+  const content = document.querySelector('#post-content').value;
+
+  if (title && content) {
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    // Redirecting user to the dashboard if response is ok
+    if (response.ok) {
+      document.location.replace('/dashboard');
     } else {
-      const titleRequirement = document.querySelector('#post-title').placeholder = 'A title is required before adding a post';
-      const contentRequirement = document.querySelector('#post-content').placeholder = 'Content cannot be left empty before adding a post';
-        
-      if(!title && !content) {
-        titleRequirement;
-        contentRequirement
-      } else if (!title) {
-        titleRequirement;
-      } else if (!content) {
-        contentRequirement
-      }
+      const error = `<p>*Failed to create post</p>`;
+      document.querySelector('.error-message').innerHTML = error;
     }
-  };
-  
-  // When the user clicks the add post button, the addPostFormHandler will be called
-  document.querySelector('#post-button').addEventListener('click', addPostFormHandler);
-  
+  } else {
+    const titleRequirement = document.querySelector('#post-title').placeholder = 'A title is required before adding a post';
+    const contentRequirement = document.querySelector('#post-content').placeholder = 'Content cannot be left empty before adding a post';
+
+    if (!title && !content) {
+      titleRequirement;
+      contentRequirement
+    } else if (!title) {
+      titleRequirement;
+    } else if (!content) {
+      contentRequirement
+    }
+  }
+};
+
+// When the user clicks the add post button, the addPostFormHandler will be called
+document.querySelector('#post-button').addEventListener('click', addPostFormHandler);
